b33bce6a94a0db201e6b0efa5d992fdd585c14de
hide-a1 <hide210g@gmail.com>
2020-05-11 11:27:53 +0900

    feat(legal):route設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
+  {
+    path: 'legal',
+    loadChildren: () =>
+      import('./legal/legal.module').then((m) => m.LegalModule),
+  },
 ];
 
 @NgModule({
--- src/app/legal/legal-routing.module.ts
+++ src/app/legal/legal-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LegalComponent } from './legal/legal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LegalComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
