bd2d542a61cca0b78c60d4c8e5949c1fc3d96565
hide-a1 <hide210g@gmail.com>
2020-05-13 08:02:02 +0900

    feat(form-routing):componentパス指定

--- src/app/form/form-routing.module.ts
+++ src/app/form/form-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: FormComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
