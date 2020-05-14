df7e85b148bf3ef1b87eff75a544f11859856ced
hide-a1 <hide210g@gmail.com>
2020-05-14 14:37:59 +0900

    feat(auth):Googleログイン追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'yama-portal';
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+    this.authService.login();
+  }
 }
