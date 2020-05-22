3561a7e74ddcd76171304793783ede39a65cde3c
hide-a1 <hide210g@gmail.com>
2020-05-22 15:13:43 +0900

    feat(home):algolia移植

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { MatIconModule } from '@angular/material/icon';
+import { NgAisModule } from 'angular-instantsearch';
+import { VisibleDirective } from '../post-list/visible.directive';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, MatIconModule],
+  declarations: [HomeComponent, VisibleDirective],
+  imports: [CommonModule, HomeRoutingModule, MatIconModule, NgAisModule],
 })
 export class HomeModule {}
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Post } from 'src/app/interfaces/post';
 import { PostService } from 'src/app/services/post.service';
+const algoliasearch = require('algoliasearch/lite');
+
+const searchClient = algoliasearch(
+  'YTCNWA1M3V',
+  '266d4f34268aad279908ca35f27a5250'
+);
 
 @Component({
   selector: 'app-home',
@@ -9,7 +15,17 @@ import { PostService } from 'src/app/services/post.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  posts$: Observable<Post[]> = this.postService.getPost();
+  posts$: Observable<Post[]> = this.postService.getPosts();
+
+  config = {
+    indexName: 'posts',
+    searchClient,
+  };
+
+  searchParams = {
+    hitsPerPage: 1,
+  };
+
   constructor(private postService: PostService) {}
 
   ngOnInit(): void {}
