374af6d613966ce07a7b617912fd7395ac4149c3
hide-a1 <hide210g@gmail.com>
2020-06-01 14:14:51 +0900

    feat(form/post):画像アップロード実装

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -7,6 +7,8 @@ import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { firestore } from 'firebase';
 import { AuthService } from './auth.service';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { async } from '@angular/core/testing';
 
 @Injectable({
   providedIn: 'root',
@@ -16,15 +18,41 @@ export class PostService {
     private db: AngularFirestore,
     private snackBar: MatSnackBar,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private storage: AngularFireStorage
   ) {}
 
-  createPost(post: Omit<Post, 'id' | 'createdAt' | 'userId'>) {
+  // createPost(post: Omit<Post, 'id' | 'createdAt' | 'userId'>) {
+  //   const id = this.db.createId();
+  //   return this.db
+  //     .doc<Post>(`posts/${id}`)
+  //     .set({
+  //       id,
+  //       createdAt: firestore.Timestamp.now(),
+  //       userId: this.authService.userId,
+  //       ...post,
+  //     })
+  //     .then(() => {
+  //       this.snackBar.open('投稿しました', null, {
+  //         duration: 2000,
+  //       });
+  //       this.router.navigateByUrl('/');
+  //     });
+  // }
+  async createPost(
+    post: Omit<Post, 'id' | 'createdAt' | 'userId'>,
+    images: {
+      imageURL: File;
+    }
+  ) {
     const id = this.db.createId();
+    const urls = await this.uploadImage(id, Object.values(images));
+    const [imageURL] = urls;
     return this.db
       .doc<Post>(`posts/${id}`)
       .set({
         id,
+        imageURL,
         createdAt: firestore.Timestamp.now(),
         userId: this.authService.userId,
         ...post,
@@ -35,6 +63,27 @@ export class PostService {
         });
         this.router.navigateByUrl('/');
       });
+  }
+
+  async uploadImage(id: string, files: File[]): Promise<string[]> {
+    if (!files) {
+      console.log('test');
+      const urls = [null];
+    } else {
+      console.log('test2');
+      return Promise.all(
+        files.map((file, index) => {
+          const ref = this.storage.ref(`posts/${id}-${index}`);
+          return ref.put(file);
+        })
+      ).then(async (tasks) => {
+        const urls = [];
+        for (const task of tasks) {
+          urls.push(await task.ref.getDownloadURL());
+        }
+        return urls;
+      });
+    }
   }
 
   getPost(): Observable<Post[]> {
