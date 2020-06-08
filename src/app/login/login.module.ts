92266a8a1f8192ae71ed8552eadf6ca8fad0b0e8
hide-a1 <hide210g@gmail.com>
2020-06-08 12:44:20 +0900

    feat(login):ボタンフォームをmaterialに変更

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,9 +4,20 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
 })
 export class LoginModule {}
