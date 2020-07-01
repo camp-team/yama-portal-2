7ab6dd2448283c6816f28e72fece0be0d6d0bd74
hide-a1 <hide210g@gmail.com>
2020-07-01 21:21:17 +0900

    fix(functions):アカウント処理用のファイルを作成・indexから移植

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,26 +1,4 @@
-import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 export * from './post.function';
-
+export * from './user';
 admin.initializeApp();
-const db = admin.firestore();
-
-export const createUser = functions
-  .region('asia-northeast1')
-  .auth.user()
-  .onCreate((user) => {
-    return db.doc(`users/${user.uid}`).set({
-      uid: user.uid,
-      name: user.displayName,
-      avaterURL: user.photoURL,
-      email: user.email,
-      createdAt: new Date(),
-    });
-  });
-
-export const deleteUser = functions
-  .region('asia-northeast1')
-  .auth.user()
-  .onDelete((user) => {
-    return db.doc(`users/${user.uid}`).delete();
-  });
