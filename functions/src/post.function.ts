b40a28001e8a2106aad4101f57590ab03c61fc92
hide-a1 <hide210g@gmail.com>
2020-07-11 07:31:08 +0900

    remove(post.function):コメントアウト削除

--- functions/src/post.function.ts
+++ functions/src/post.function.ts
@@ -41,33 +41,3 @@ export const updatePost = functions
       data,
     });
   });
-// import * as functions from 'firebase-functions';
-// const algoliasearch = require('algoliasearch');
-// import * as admin from 'firebase-admin';
-
-// const ALGOLIA_ID = functions.config().algolia.app_id;
-// const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
-// const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
-// const index = client.initIndex('posts');
-
-// export const addRecord = functions
-//   .region('asia-northeast1')
-//   .firestore.document('posts/{id}')
-//   .onCreate((snap, context) => {
-//     const data = snap.data() as {
-//       id: string;
-//       userId: string;
-//       imageURL: string;
-//       label: string;
-//       content: string;
-//       public: boolean;
-//       createdAt: admin.firestore.Timestamp;
-//     };
-//     const item = {
-//       ...data,
-//       objectID: data.id,
-//       createdAt: data.createdAt.toMillis(),
-//     };
-
-//     return index.saveObject(item);
-//   });
