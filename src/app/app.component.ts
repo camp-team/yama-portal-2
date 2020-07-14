ceebd9796399352deba8ca79514bcad82d66d590
hide-a1 <hide210g@gmail.com>
2020-07-14 11:30:10 +0900

    feat(app):サイドナビレスポンシブ対応

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,31 +4,45 @@ import {
   ElementRef,
   OnInit,
   AfterViewInit,
+  ChangeDetectorRef,
+  OnDestroy,
 } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { DrawerService } from './services/drawer.service';
 import { UiService } from './services/ui.service';
 import { MatSidenav, MatSidenavContent } from '@angular/material/sidenav';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnDestroy {
   @ViewChild('wrap') private wrap: MatSidenavContent;
+  mobileQuery: MediaQueryList;
 
   title = 'yama-portal';
   user$ = this.authService.user$;
   opened: boolean;
 
+  private _mobileQueryListener: () => void;
+
   constructor(
     private authService: AuthService,
     private drawerService: DrawerService,
-    private uiService: UiService
+    private uiService: UiService,
+    private changeDetectorRef: ChangeDetectorRef,
+    private media: MediaMatcher
   ) {
     this.drawerService.toggle();
     this.drawerService.isOpen$.subscribe((opened) => (this.opened = opened));
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
   logout() {
