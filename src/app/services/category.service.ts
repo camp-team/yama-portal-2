3566630bbd1ea43efd3d1e562f0b00a2cb5434fb
hide-a1 <hide210g@gmail.com>
2020-07-16 16:50:56 +0900

    feat(category):category.service作成

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../interfaces/category';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoryService {
-  constructor() {}
+  constructor(private db: AngularFirestore) {}
+
+  getCategories(): Observable<Category[]> {
+    return this.db.collection<Category>('categories').valueChanges();
+  }
+
+  getCategoriesLatest(): Promise<Category[]> {
+    return this.getCategories().pipe(take(1)).toPromise();
+  }
 }
