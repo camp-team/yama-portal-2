2b8f27407d17ff83c494aee9625a4275b063ed35
hide-a1 <hide210g@gmail.com>
2020-07-16 18:45:05 +0900

    remove:不要箇所削除

--- src/app/shared/filter/filter.component.ts
+++ src/app/shared/filter/filter.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from 'src/app/services/search.service';
 import { FormControl } from '@angular/forms';
-import { MatSelectionListChange, MatListOption } from '@angular/material/list';
 import { Router } from '@angular/router';
-import { CategoryService } from 'src/app/services/category.service';
-import { Category } from 'src/app/interfaces/category';
 import { MatRadioChange } from '@angular/material/radio';
 
 @Component({
@@ -15,63 +12,29 @@ import { MatRadioChange } from '@angular/material/radio';
 export class FilterComponent implements OnInit {
   private index = this.searchService.index.posts;
   uplaodAtFilter = new FormControl('');
-  tags: {
+  categories: {
     value: string;
     highlighted: string;
     count: number;
     selected?: boolean;
   }[];
 
-  categories: Category[];
-
   visible: boolean;
 
-  constructor(
-    private searchService: SearchService,
-    private router: Router,
-    private categoryService: CategoryService
-  ) {}
+  constructor(private searchService: SearchService, private router: Router) {}
 
   ngOnInit(): void {
-    this.buildTags();
     this.buildCategories();
   }
 
-  private buildTags() {
-    this.index.searchForFacetValues('category', '').then((res) => {
-      this.tags = res.facetHits;
-    });
-  }
-
   private buildCategories() {
-    this.categoryService.getCategoriesLatest().then((res) => {
-      this.categories = res;
+    this.index.searchForFacetValues('category', '').then((res) => {
+      this.categories = res.facetHits;
     });
   }
 
   toggle() {
     this.visible = !this.visible;
-  }
-
-  buildQueryParameterByCreatedAt(event: MatSelectionListChange) {
-    const key: MatListOption = event.source.selectedOptions.selected[0].value;
-    this.router.navigate([''], {
-      queryParams: {
-        createdAt: key,
-      },
-      queryParamsHandling: 'merge',
-    });
-  }
-
-  buildQueryParameterByTags(event: MatSelectionListChange) {
-    const options: MatListOption[] = event.source.selectedOptions.selected;
-    const tagsFileter = options.map((option) => option.value).join(',') || null;
-    this.router.navigate([''], {
-      queryParams: {
-        tags: tagsFileter,
-      },
-      queryParamsHandling: 'merge',
-    });
   }
 
   buildQueryParameterByCategories(event: MatRadioChange) {
