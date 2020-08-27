502b1f2645f87bb0ac8a134b362b6b29e84be1b8
hide-a1 <hide210g@gmail.com>
2020-08-27 13:59:27 +0900

    feat(stripe): stripeセットアップ

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  stripe: {
+    publicKey:
+      'pk_live_51HKazNHfsoP9rVSq4PBz1u2aF0lfEP6BhSdTYjsIdBFnXOw8EWWRavzEi1NhmCPixOobVYfyM4xcudyRZNcpXywT00Iq9V3XEa',
+  },
 };
