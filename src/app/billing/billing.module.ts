09e38bd17f4f510a818d8654000538258fc42eff
hide-a1 <hide210g@gmail.com>
2020-09-07 11:33:20 +0900

    feat(billing): 決済用ベージ作成

--- src/app/billing/billing.module.ts
+++ src/app/billing/billing.module.ts
@@ -6,9 +6,16 @@ import { BillingComponent } from './billing/billing.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { SharedModule } from '../shared/shared.module';
+import { InvoiceComponent } from './invoice/invoice.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [BillingComponent],
+  declarations: [BillingComponent, InvoiceComponent],
   imports: [
     CommonModule,
     BillingRoutingModule,
@@ -16,6 +23,12 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
     MatInputModule,
     ReactiveFormsModule,
     FormsModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    SharedModule,
+    MatListModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
   ],
 })
 export class BillingModule {}
--- src/app/billing/billing/billing.component.ts
+++ src/app/billing/billing/billing.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PaymentService } from 'src/app/services/payment.service';
 import { FormBuilder, Validators, FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
+import { CreditCardComponent } from 'src/app/shared/dialogs/credit-card/credit-card.component';
+import Stripe from 'stripe';
+import { ConfirmDialogComponent } from 'src/app/shared/dialogs/confirm-dialog/confirm-dialog.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-billing',
   templateUrl: './billing.component.html',
   styleUrls: ['./billing.component.scss'],
 })
-export class BillingComponent implements OnInit {
+export class BillingComponent implements OnInit, OnDestroy {
+  public paymentMethod: Stripe.PaymentMethod;
+
+  private subscriptions: Subscription = new Subscription();
+
   donationForm = this.fb.group({
     donationAmount: [
       '',
@@ -30,5 +38,44 @@ export class BillingComponent implements OnInit {
     private userService: UserService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getCard();
+  }
+
+  openCreditCardDialog() {
+    const dialogRef = this.dialog.open(CreditCardComponent);
+    this.subscriptions = dialogRef.afterClosed().subscribe(() => {
+      this.getCard();
+    });
+  }
+
+  openConfirmDialog() {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent);
+    this.subscriptions = dialogRef
+      .afterClosed()
+      .subscribe((result: boolean) => {
+        if (result) {
+          this.donate();
+        }
+      });
+  }
+
+  getCard() {
+    console.log('test');
+    this.paymentService.getPaymentMethod().then((methods) => {
+      console.log(methods);
+      if (methods) {
+        this.paymentMethod = methods.data[0];
+        console.log(this.paymentMethod);
+      }
+    });
+  }
+
+  private donate() {
+    this.paymentService.donate(this.donationForm.value.donationAmount);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
