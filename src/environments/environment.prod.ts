6140c42f31dfaff96e45ac0d7d58dc44026045bd
hide-a1 <hide210g@gmail.com>
2020-09-15 16:03:42 +0900

    feat: 開発環境と本番環境を分ける Fixes #152

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,21 @@
 export const environment = {
   production: true,
+  algolia: {
+    appId: 'D27EC94D9Q',
+    searchKey: '5c5b0059d0b529b44116295de419aacb',
+  },
   stripe: {
     publicKey:
       'pk_live_51HKazNHfsoP9rVSq4PBz1u2aF0lfEP6BhSdTYjsIdBFnXOw8EWWRavzEi1NhmCPixOobVYfyM4xcudyRZNcpXywT00Iq9V3XEa',
+  },
+  firebase: {
+    apiKey: 'AIzaSyAQMCisAfCKuqeq2I6b-xD-j1wRl4vXh4U',
+    authDomain: 'yamapath-3d9c1.firebaseapp.com',
+    databaseURL: 'https://yamapath-3d9c1.firebaseio.com',
+    projectId: 'yamapath-3d9c1',
+    storageBucket: 'yamapath-3d9c1.appspot.com',
+    messagingSenderId: '428992344434',
+    appId: '1:428992344434:web:8670f15e04f5078a246deb',
+    measurementId: 'G-FTFWNSPZTW',
   },
 };
