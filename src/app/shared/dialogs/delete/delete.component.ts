ce37716173114e44c21bf1e62767170806e08636
hide-a1 <hide210g@gmail.com>
2020-09-23 16:50:17 +0900

    feat(delete): 削除用ダイアログ作成

---
+++ src/app/shared/dialogs/delete/delete.component.ts
@@ -0,0 +1,13 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-delete',
+  templateUrl: './delete.component.html',
+  styleUrls: ['./delete.component.scss'],
+})
+export class DeleteComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
